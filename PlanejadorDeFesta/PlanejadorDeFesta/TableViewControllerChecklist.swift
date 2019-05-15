751e5a7314d3315e468833723f7bdf8bd7b87dde
Juliana Vigato Pavan <juvigato@10-44-48-199.0200.mack.br.iosda.org>
2019-05-15 13:29:30 -0300

    atualizando

---
+++ PlanejadorDeFesta/PlanejadorDeFesta/TableViewControllerChecklist.swift
@@ -0,0 +1,55 @@
+//
+//  TableViewControllerChecklist.swift
+//  PlanejadorDeFesta
+//
+//  Created by Juliana Vigato Pavan on 13/05/19.
+//  Copyright © 2019 Juliana Vigato Pavan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewControllerChecklist: UITableViewController{
+    if .checkmark != .none{
+        //pegar info de pessoas bebadas
+    }
+    if jantar -> checkmark sim{
+        menos salgadinhos
+    }
+    
+    -------
+    
+    Bebidas:
+    (250*party.drunkpeople*tempo) a (375*party.drunkpeople*tempo)
+    +
+    (250*nConvidados*tempo) a (375*nConvidados*tempo)
+    
+    
+    
+
+    Comida:
+    
+    - Com jantar: (if text.label == "sim")
+    5 salgadinhos * nConvidados * tempo
+    +
+    150g carne * nConvidados
+    +
+    50g acompanhamento * nConvidados
+    +
+    5 doces * nConvidados
+    +
+    1 prato doce * nConvidados
+    
+    
+    - Sem refeicao:
+    10 salgadinhos * nConvidados * tempo
+    +
+    5 doces * nConvidados
+    +
+    1 prato doce * nConvidados
+    
+    
+    
+}
+
+
